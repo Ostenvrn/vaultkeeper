9e05977e1824e3274e23f8bcf9f9f00ed0ac23e4
OsteNvrn <ostenvrn@gmail.com>
2026-09-09 16:38:02 +0300

    scanner.py

--- src/scanner.py
+++ src/scanner.py
@@ -39,7 +39,7 @@ class SystemScanner:
                 if os.path.exists(file_path):
                     try:
                         with open(file_path, 'r') as f:
-                            result["configs"][file_path] = f.read(1000)  # Первые 1000 символов
+                            result["configs"][file_path] = f.read()  # читаем полностью
                     except:
                         result["configs"][file_path] = "Ошибка чтения"
         
